566c13cd87f9e0652534c61f77f8c9f2c6e217cf
moyan <1561515308@qq.com>
2025-07-21 13:57:53 +0800

    initial commit

---
+++ emctl-cli/src/main.rs
@@ -0,0 +1,8 @@
+use emctl_core;
+
+fn main() {
+    println!(
+        "Hello, world! This is server version: {}",
+        emctl_core::SERVER_VERSION
+    );
+}
---
+++ emctl-core/src/lib.rs
@@ -0,0 +1,1 @@
+pub const SERVER_VERSION: &str = "0.1.0";
